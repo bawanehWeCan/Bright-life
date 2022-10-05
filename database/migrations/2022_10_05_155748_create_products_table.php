5138fb25d6b64d9e271a070dca97103fa53be2bd
Alaa Bawaneh <a.bawaneh@wecan.jo>
2022-10-05 20:22:56 +0300

    fix database

--- database/migrations/2022_10_05_155748_create_products_table.php
+++ database/migrations/2022_10_05_155748_create_products_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->id();
             $table->text('name');
             $table->text('content');
             $table->double('price');
@@ -24,6 +23,7 @@ return new class extends Migration
             $table->text('image')->nullable();
             $table->timestamps();
             $table->timestamps();
+
         });
     }
 

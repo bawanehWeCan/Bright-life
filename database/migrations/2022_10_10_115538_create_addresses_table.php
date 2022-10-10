e47fe515e5497347c036ddfc7dbacf5f81ea9d7c
Alaa Bawaneh <a.bawaneh@wecan.jo>
2022-10-10 16:46:42 +0300

    address

--- database/migrations/2022_10_10_115538_create_addresses_table.php
+++ database/migrations/2022_10_10_115538_create_addresses_table.php
@@ -15,14 +15,15 @@ return new class extends Migration
     {
         Schema::create('addresses', function (Blueprint $table) {
             $table->id();
-            $table->text('name');
-            $table->text('region');
-            $table->text('street');
-            $table->text('building_number');
-            $table->text('floor');
-            $table->text('apartment_number');
-            $table->text('additional_tips');
-            $table->text('phone_number');
+            $table->text('name')->nullable();
+            $table->text('region')->nullable();
+            $table->text('street')->nullable();
+            $table->text('building_number')->nullable();
+            $table->text('floor')->nullable();
+            $table->text('apartment_number')->nullable();
+            $table->text('additional_tips')->nullable();
+            $table->text('phone_number')->nullable();
+            $table->integer('user_id')->nullable();
             $table->timestamps();
         });
     }

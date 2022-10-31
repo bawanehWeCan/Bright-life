e05659d2672afad87b1fbbff8cdc8035ad614143
aya-matroud <ayamatroud8@gmail.com>
2022-10-31 05:37:36 -0700

    addReviews

---
+++ app/Http/Controllers/Api/ReviewController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Review;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\ApiController;
+use App\Http\Requests\ReviewRequest;
+use Illuminate\Http\Request;
+use App\Repositories\Repository;
+use App\Http\Resources\ReviewResource;
+
+class ReviewController extends ApiController
+{
+    public function __construct()
+    {
+        $this->resource = ReviewResource::class;
+        $this->model = app(Review::class);
+        $this->repositry =  new Repository($this->model);
+    }
+
+    public function test(){
+        return $this->list();
+    }
+
+    public function save(ReviewRequest $request ){
+        return $this->store( $request->all() );
+    }
+
+
+
+}

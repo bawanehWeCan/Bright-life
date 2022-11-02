9c22fe780f73dbc47a3f8cd1cb7e764a58284ebc
Alaa Bawaneh <a.bawaneh@wecan.jo>
2022-11-02 17:00:14 +0200

    fix

--- app/Http/Resources/OrderResource.php
+++ app/Http/Resources/OrderResource.php
@@ -32,7 +32,7 @@ class OrderResource extends JsonResource
             'number'=>$this->number,
             'order_value'=>number_format($this->order_value,2),
             'user'=>new UserResource( User::findOrFail($this->user_id) ),
-            'address'=>implode(", ", array_filter(collect(new AddressItemResource( $this->user->address->first() ))->toArray())),
+            'address'=>new AddressItemResource( $this->user->address->first() ),
             'supplier_id'=>new UserResource( User::findOrFail($this->supplier_id) ),
             'products'=> CartItemResource::collection($this->products),
         ];

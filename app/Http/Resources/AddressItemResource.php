0196d2a9dfdab1c65116e590a4e06caaeeaa2664
Alaa Bawaneh <a.bawaneh@wecan.jo>
2022-11-02 17:13:41 +0200

    fix

--- app/Http/Resources/AddressItemResource.php
+++ app/Http/Resources/AddressItemResource.php
@@ -15,12 +15,13 @@ class AddressItemResource extends JsonResource
     public function toArray($request)
     {
         return [
-            $this->city,
-            $this->region,
-            $this->street,
-            $this->building_number,
-            $this->floor_number,
-            $this->apartment_number,
+            "city" => $this->city,
+            "name" => $this->name,
+            "region" => $this->region,
+            "street" => $this->street,
+            "building_number" => $this->building_number,
+            "floor_number" => $this->floor_number,
+            "apartment_number" => $this->apartment_number,
         ];
     }
 }

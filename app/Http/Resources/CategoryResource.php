320657e829eef81ee73fffd39aaa1a496fd67b90
Alaa Bawaneh <a.bawaneh@wecan.jo>
2022-11-15 16:49:32 +0300

    rec

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use App\Models\User;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\DB;
 
 class CategoryResource extends JsonResource
 {
@@ -20,8 +21,16 @@ class CategoryResource extends JsonResource
             'name'=>$this->name,
             'image'=>$this->image,
             'type'=>$this->type,
-            'best'=>SupplierResource::collection(User::where('type', 'supplier')->orderBy('points','desc')->get()),
+            'best'=>SupplierResource::collection(User::with([ 'review'])
+            ->leftJoin('reviews', 'reviews.supplier_id', '=', 'users.id')
+            ->select(['products.*',
+                DB::raw('AVG(reviews.points) as ratings_average')
+                ])
+            ->orderBy('ratings_average', 'desc')
+            ->get()),
             'suppliers'=>SupplierResource::collection($this->suppliers),
         ];
     }
 }
+
+

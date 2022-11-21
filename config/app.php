3b0400909c7e60cc3098ee4e253a3f4e311e3553
Alaa Bawaneh <a.bawaneh@wecan.jo>
2022-11-21 18:10:24 +0300

    aaa

--- config/app.php
+++ config/app.php
@@ -195,7 +195,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Darryldecode\Cart\CartServiceProvider::class
+        Darryldecode\Cart\CartServiceProvider::class,
+        Kreait\Laravel\Firebase\ServiceProvider::class
 
     ],
 
